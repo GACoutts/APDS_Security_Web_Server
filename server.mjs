4f3bed4c12cc735fe5524245172432be61868b58
GACoutts <ST10258297@imconnect.edu.za>
2025-08-05 16:08:47 +0200

    Made a hhtp server and assigned a port

--- server.mjs
+++ server.mjs
@@ -1,1 +1,12 @@
-console.log('APDS makes me cry')
+import http from "http"
+//setting port thayt server listens to
+const PORT = 3000;
+
+//starting server 
+//req = requeust
+//res = response
+const server = http.createServer((req,res) => {
+    res.end('I can wriyte anything! MUAHAHAHHAHAHAHAHAHA')
+})
+
+server.listen(PORT)
