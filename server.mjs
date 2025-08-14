3a3eb748548806f914fd0646b6f166083e2a38d1
GACoutts <ST10258297@imconnect.edu.za>
2025-08-14 16:34:29 +0200

    JSON example used wuth fruits: Message of fruits then the json list

--- server.mjs
+++ server.mjs
@@ -19,15 +19,40 @@ import express from 'express';
 
 const PORT = 3000;
 const app = express();
+const urlPrefix = '/api'
 
 app.use(express.json());
 
+//this '/' is the root for the site
 app.get('/',(req,res) => {
 res.send('How will this work for graphics?!?!?!?!?. Tetsting changes');
 });
 
-app.get('/test', (req, res)=>{
-    res.send('Endpoint testing with /test endpoint');
+app.get(urlPrefix + '/orders', (req, res)=>{
+    const orders = [
+        {
+            id: "1",
+            name: "apple"
+        },
+        {
+            id: "2",
+            name: "orange"
+        },
+        {
+            id: "3",
+            name: "pear"
+        },
+        {
+            id: "4",
+            name: "banana"
+        }
+    ]
+    res.json(
+        {
+            message: "Fruit",
+            orders: orders
+        }
+    )
 });
 
 app.listen(PORT);
