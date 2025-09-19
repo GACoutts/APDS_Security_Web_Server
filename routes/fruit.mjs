3ce832f0469865d7dbb7dcefdb92e4277d7972ea
GACoutts <ST10258297@imconnect.edu.za>
2025-09-19 16:25:02 +0200

    Working update, delete, and find by id

--- routes/fruit.mjs
+++ routes/fruit.mjs
@@ -14,6 +14,7 @@ router.get("/", async(req,res) => {
 
 //creating of a record
 //Doesnt this need siyte input??
+// gets input from post method under hhtp.localhost:3000/fruit/fruit
 router.post("/fruit", async(req,res) => {
 let newDocument = {
     name: req.body.name,
@@ -24,4 +25,39 @@ let results = await collection.insertOne(newDocument);
 res.send(results).status(204);
 });
 
+//this is to update a single thing by id
+router.patch("/:id", async(req,res) => {
+    const query = {_id: new ObjectId(req.params.id)};
+    const updates = {
+        $set: {
+            name: req.body.name,
+            comment: req.body.comment
+        }
+    };
+
+    let collection = await db.collection("fruit");
+    let result = await collection.updateOne(query,updates);
+
+    res.send(result).status(200)
+});
+
+//gets single record by id
+router.get("/:id", async(req,res) => {
+    let collection = await db.collection("fruit");
+    let query = {_id: new ObjectId(req.params.id)};
+    let result = await collection.findOne(query);
+
+    if(!result) res.send("not found").status(404);
+    else res.send(result).status(200);
+});
+
+//deleteing by one
+router.delete("/:id", async(req,res) => {
+    const query = {_id: new ObjectId(req.params.id)};
+
+    const collection = await db.collection("fruit");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+})
 export default router;
