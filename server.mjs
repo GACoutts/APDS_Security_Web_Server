2a5f36182c525654691c6cfa9b192aa2a9170a17
GACoutts <ST10258297@imconnect.edu.za>
2025-09-21 11:37:47 +0200

    Switch to port.mjs instead of fruit

--- server.mjs
+++ server.mjs
@@ -1,8 +1,8 @@
 import https from "https";
-import http from "http";
 //gives us the ability to read and right files(how we do the . pem files)
 import fs from "fs";
 import fruits from "./routes/fruit.mjs";
+import posts from "./routes/posts.mjs";
 import express from "express";
 //cross origin resource sharing
 //secuirty feature that prebents web pages form making request to other things
@@ -30,11 +30,14 @@ app.use((req,res,next) => {
     next();
 });
 //using route model for basis
-app.use("/fruit", fruits);
+//app.use("/fruit", fruits);
 //set up the route for fruits
-app.route("/fruit", fruits);
+//app.route("/fruit", fruits);
+app.use("/posts", posts);
+app.route("/posts", posts);
 
 //options needs to go through the create server for the keys stuff
 let server = https.createServer(options, app);
+console.log(PORT);
 
 server.listen(PORT);
